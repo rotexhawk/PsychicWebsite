aef5862d4706628a12ab44666b54b107917d1c11
Yasin Yaqoobi <yasinyaqoobi@gmail.com>
2018-06-04 23:55:55 -0400

    this might be a game changer

--- client/src/containers/PsychicCards/index.js
+++ client/src/containers/PsychicCards/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 
-const PsychicCards = () => (<h1>This is working</h1>);
+const PsychicCards = () => (<h1>This might be a game changer</h1>);
 
 export default PsychicCards;
